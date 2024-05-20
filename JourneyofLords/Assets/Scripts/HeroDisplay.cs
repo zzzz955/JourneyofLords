79abf28cd2d850eb4095617bac44ef050f188006
Sang <zzzzz955@gmail.com>
2024-05-20 19:24:43 +0900

    영웅UI 스크롤 뷰 조정
    영웅UI 스크롤 뷰 이미지 밖으로 나가지 않도록 조정

--- JourneyofLords/Assets/Scripts/HeroDisplay.cs
+++ JourneyofLords/Assets/Scripts/HeroDisplay.cs
@@ -5,22 +5,34 @@ using TMPro;
 public class HeroDisplay : MonoBehaviour
 {
     public Image spriteImage;
+    public Image lvImage;
     public TMP_Text levelText;
 
     public void SetHeroData(Hero hero)
     {
         // 스프라이트 설정
-        Sprite sprite = Resources.Load<Sprite>(hero.spriteName);
-        if (sprite != null)
+        Sprite sprite1 = Resources.Load<Sprite>(hero.spriteName);
+        if (sprite1 != null)
         {
-            spriteImage.sprite = sprite;
+            spriteImage.sprite = sprite1;
         }
         else
         {
             Debug.LogError("Failed to load sprite: " + hero.spriteName);
         }
 
+        Sprite sprite2 = Resources.Load<Sprite>(hero.att);
+        if (sprite2 != null)
+        {
+            lvImage.sprite = sprite2;
+        }
+        else
+        {
+            Debug.LogError("Failed to load sprite: " + hero.att);
+        }
+
         // 텍스트 필드 설정
         levelText.SetText(hero.level.ToString());
+        
     }
 }
